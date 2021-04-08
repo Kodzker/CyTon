b00480adf457e2517b5099355a6fd4b92bce4d68
Kodzker <72316569+Kodzker@users.noreply.github.com>
2021-04-08 22:41:13 +0300

    Add new files
    Signed-off-by: CyTon Code <cytoncode@gmail.com>

---
+++ syntax.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;   //std::.....
+
+
+#define start vector<char*> args; int main (int args_length, char* args_array[])
+
+#define begin int process = 0
+
+// for(int i=0;i<args_length;i++)
+// args.push_back(args_array[i])
+
+
+#define end cout << "[Program Finished]"; return (process)
+
+
