990bc245177aa87a6b47b5f27d2c4fbbbe7ae63e
CyTon Code <cytoncode@gmail.com>
2021-04-11 00:46:16 +0300

    добавил новый класс, означает об использовании имени которого нет.

---
+++ exc/NameItsNotDefined.h
@@ -0,0 +1,16 @@
+//
+// Created by cyton_code on 11.04.2021.
+//
+
+#ifndef CYTON_NAME_ITS_NOT_DEFINED_H
+#define CYTON_NAME_ITS_NOT_DEFINED_H
+
+
+class NameItsNotDefined : std::exception {
+public:
+    // Name it is not defined
+    NameItsNotDefined() = default;
+};
+
+
+#endif //CYTON_NAME_ITS_NOT_DEFINED_H
